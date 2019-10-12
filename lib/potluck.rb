31dc79f0dc4c69b59759ed29554a670f280c6f98
Jesse Higgins <jessofjames@gmail.com>
2019-10-11 21:35:29 -0600

    Adds array shovel method for @dishes array and adds :add_dish attr_reader.

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,7 +1,11 @@
 class Potluck
-  attr_reader :date, :dishes
+  attr_reader :date, :dishes, :add_dish
   def initialize(date)
     @date = date
     @dishes = []
+  end
+
+  def add_dish(dish)
+    @dishes << dish
   end
 end
