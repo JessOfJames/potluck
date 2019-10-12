e63b08f1593c9b1503533dce9c8bb5a7404a4d89
Jesse Higgins <jessofjames@gmail.com>
2019-10-11 20:25:34 -0600

    Adds functionality to has_name and has_category test methods. All tests pass.

--- test/dish_test.rb
+++ test/dish_test.rb
@@ -11,11 +11,11 @@ class DishClass < Minitest::Test
 
   def test_dish_has_a_name
     dish = Dish.new("Couscous Salad", :appetizer)
-    assert_equal "Couscous Salad", name
+    assert_equal "Couscous Salad", dish.name
   end
 
   def test_dish_has_a_category
     dish = Dish.new("Couscous Salad", :appetizer)
-    assert_equal ":appetizer", category
+    assert_equal :appetizer, dish.category
   end
 end
