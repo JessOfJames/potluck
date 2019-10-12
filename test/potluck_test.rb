0fe15ab93e274aafef2c8f8a2caa3da795d070fe
Jesse Higgins <jessofjames@gmail.com>
2019-10-11 20:40:53 -0600

    Adds setup method and has_date test. All tests pass.

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -4,8 +4,14 @@ require './lib/dish'
 require './lib/potluck'
 
 class PotluckClass < Minitest::Test
+  def setup
+    @potluck = Potluck.new("7-13-18")
+  end
   def test_potluck_class_exists
-    potluck = Potluck.new("7-13-18")
-    assert_instance_of Potluck, potluck
+    assert_instance_of Potluck, @potluck
+  end
+
+  def test_potluck_has_date
+    assert_equal "7-13-18", @potluck.date
   end
 end
