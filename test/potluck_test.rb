621abcae6fbab584d71823a6ef070b63cb251df0
Jesse Higgins <jessofjames@gmail.com>
2019-10-11 21:37:16 -0600

    Adds dishes? and add_dishes? tests. All tests pass.

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -6,6 +6,8 @@ require './lib/potluck'
 class PotluckClass < Minitest::Test
   def setup
     @potluck = Potluck.new("7-13-18")
+    @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    @cocktail_meatball = Dish.new("Cocktail Meatballs", :entre)
   end
   def test_potluck_class_exists
     assert_instance_of Potluck, @potluck
@@ -13,5 +15,15 @@ class PotluckClass < Minitest::Test
 
   def test_potluck_has_date
     assert_equal "7-13-18", @potluck.date
+  end
+
+  def test_potluck_has_dish
+    assert_equal [], @potluck.dishes
+  end
+
+  def test_dishes_can_be_added_to_potluck
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@cocktail_meatball)
+    assert_equal 2, @potluck.dishes.length
   end
 end
