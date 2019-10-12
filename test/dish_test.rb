76205fed364b2689bd6fb770be1efee5f97ee9a5
Jesse Higgins <jessofjames@gmail.com>
2019-10-11 20:19:45 -0600

    Adds test_for_name and test_for_category type testing. Test do not pass.

--- test/dish_test.rb
+++ test/dish_test.rb
@@ -4,8 +4,18 @@ require './lib/dish'
 
 class DishClass < Minitest::Test
 
-  def test_it_exists
+  def test_dish_exists
     dish = Dish.new("Couscous Salad", :appetizer)
     assert_instance_of Dish, dish
+  end
+
+  def test_dish_has_a_name
+    dish = Dish.new("Couscous Salad", :appetizer)
+    assert_equal "Couscous Salad", name
+  end
+
+  def test_dish_has_a_category
+    dish = Dish.new("Couscous Salad", :appetizer)
+    assert_equal ":appetizer", category
   end
 end
